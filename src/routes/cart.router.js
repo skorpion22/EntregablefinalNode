489cc643bfb7979bfab797ca771fb66157aace26
Droldan <dieginho.r222@gmail.com>
2024-02-23 08:53:54 -0500

    advances

---
+++ src/routes/cart.router.js
@@ -0,0 +1,14 @@
+const { getAll, create, remove, update } = require('../controllers/cart.controllers');
+const express = require('express');
+
+const routerCart = express.Router();
+
+routerCart.route('/')
+  .get(getAll)
+  .post(create);
+
+routerCart.route('/:id')
+  .delete(remove)
+  .put(update);
+
+module.exports = routerCart;
